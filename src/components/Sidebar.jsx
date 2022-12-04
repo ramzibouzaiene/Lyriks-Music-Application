f04d7ee802a41fd8491de5b548aef901408b3425
ramzibouzaiene <ramzibouzaiene00@gmail.com>
2022-12-04 21:47:15 +0100

    adding top plays songs top charts top artists and all the styles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -32,8 +32,8 @@ const Sidebar = () => {
       </div>
       <div className="absolute md:hidden block top-6 right-3">
         {mobileMenuOpen ? (
-          <RiCloseLine className="w-6 h-6 text-white mr-2" handleClick={() => setMobileMenuOpen(false)} />
-        ) : <HiOutlineMenu className="w-6 h-6 text-white mr-2" handleClick={() => setMobileMenuOpen(true)} />}
+          <RiCloseLine className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(false)} />
+        ) : <HiOutlineMenu className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(true)} />}
       </div>
       <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b]
       backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}
